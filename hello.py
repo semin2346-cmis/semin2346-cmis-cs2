ce33acf04fcc7a90f513f51d018b55e2fd0c8089
cmis <cmis@cmis-hs13.(none)>
2016-01-27 11:12:26 +0700

    Python practice assignment.

---
+++ hello.py
@@ -0,0 +1,12 @@
+print "Hello, World!"
+print "Se Min Park."
+print "NHD IS COMING 'ROUND THE CORNER TIME TO WRITE THE ESSAY..."
+print "ONLY 23 DAYS LEFT."
+print True
+print False
+print 2
+print 0.2354678436575432
+print 23 DAYS. SEND HELP."
+print ("Mr. Brian Haynes", "NHD", "Annexations", "GO HOME", 23)
+print ("Mr. David Craig", "English", "Propaganda", 1930)
+print ("Mrs. Naw Paw Tah Shu", "Pre Calculus", "Geometric series", -234.444552)
