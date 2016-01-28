c8f81065f7f5c1d82f889bed7b3e71fe7b2e045d
cmis <cmis@cmis-hs13.(none)>
2016-01-28 14:34:21 +0700

    Modified for the worse.

--- hello.py
+++ hello.py
@@ -1,12 +1,27 @@
 print "Hello, World!"
 print "Se Min Park."
 print "NHD IS COMING 'ROUND THE CORNER TIME TO WRITE THE ESSAY..."
-print "ONLY 23 DAYS LEFT."
+print "ONLY 22 DAYS LEFT."
 print True
 print False
 print 2
 print 0.2354678436575432
-print 23 DAYS. SEND HELP."
+print (22, "DAYS. SEND HELP.")
 print ("Mr. Brian Haynes", "NHD", "Annexations", "GO HOME", 23)
-print ("Mr. David Craig", "English", "Propaganda", 1930)
+print ("Mr. David Craig", "English", "Propaganda", 1914)
 print ("Mrs. Naw Paw Tah Shu", "Pre Calculus", "Geometric series", -234.444552)
+myName = "Se Min Park"
+myDream = "Finish NHD successfully."
+myGoal = "English propaganda speech..."
+myAP = "Oh my goodness it's just around the corner it's advancing..."
+print myName
+print myDream
+print myGoal
+print 3*myAP
+print 3 + 2222223343234
+print 3.45633 - 5.4
+print 3 * 5
+print 5/9
+print 3 ** 4
+print 10 % 2
+print 10 // 2
