543e259d484b2312ff09f0f5fd66e534d6286812
cmis <cmis@cmis-hs13.(none)>
2016-02-01 09:54:34 +0700

    Assignment 1. Such a proud creator.

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+myName = "Se Min Park"
+myAgeindecimals = 16.36
+myHeightinmeters = 1.59
+sidelengthofSquare = 4
+lengthofRectangle = 6
+heightofRectangle = 2
+myAgeinmonths = myAgeindecimals * 12
+numberofyearsIhaveleft = 85 - myAgeindecimals
+myHeightinfeet = myAgeindecimals * 3.28084
+differenceofmyHeightandKoreanaverage = 1.625 - myHeightinmeters
+areaofSquare = sidelengthofSquare ** 2
+halfthevolumeofCubeusingmySquaredimensions = 0.5 * (sidelengthofSquare ** 3)
+ninethofRectanglearea = (1/9) * (lengthofRectangle * heightofRectangle)
+print "I am " + str(myName) + "." " I am " + str(myAgeindecimals) + " years old" + "." " My square has the sidelength of " + str(sidelengthofSquare) + " units" + "." " I have " + str(numberofyearsIhaveleft) + " years left to live" + "." " My height (meters) corresponds to " + str(myHeightinmeters) + "." " I am a short person" + "."
