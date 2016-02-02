21e96eae8ca24a7809391137e24efa331323bbd3
cmis <cmis@cmis-hs13.(none)>
2016-02-02 11:27:28 +0700

    Finished assignment.

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,16 @@
-myName = "Se Min Park"
-myAgeindecimals = 16.36
-myHeightinmeters = 1.59
-sidelengthofSquare = 4
-lengthofRectangle = 6
-heightofRectangle = 2
-myAgeinmonths = myAgeindecimals * 12
-numberofyearsIhaveleft = 85 - myAgeindecimals
-myHeightinfeet = myAgeindecimals * 3.28084
-differenceofmyHeightandKoreanaverage = 1.625 - myHeightinmeters
-areaofSquare = sidelengthofSquare ** 2
-halfthevolumeofCubeusingmySquaredimensions = 0.5 * (sidelengthofSquare ** 3)
-ninethofRectanglearea = (1/9) * (lengthofRectangle * heightofRectangle)
-print "I am " + str(myName) + "." " I am " + str(myAgeindecimals) + " years old" + "." " My square has the sidelength of " + str(sidelengthofSquare) + " units" + "." " I have " + str(numberofyearsIhaveleft) + " years left to live" + "." " My height (meters) corresponds to " + str(myHeightinmeters) + "." " I am a short person" + "."
+myName = "Se Min Park" #This is the variable for my name given to me at birth.
+myAgeindecimals = 16.36 #This is the variable for my current age in decimal formation.
+myHeightinmeters = 1.59 #This is the variable for my weirdly short height in meters.
+sidelengthofSquare = 4 #This is the variable for my square.
+lengthofRectangle = 6 #This is the variable for the length of my rectangle.
+heightofRectangle = 2 #This is the variable for the height of my rectangle.
+myAgeinmonths = myAgeindecimals * 12 #This is the variable for my current age in months.
+numberofyearsIhaveleft = 85 - myAgeindecimals #This is the variable for the average years I have left to live.
+myHeightinfeet = myAgeindecimals * 3.28084 #This is the variable for my weirdly short height in feet because Americans still use the imperial system.
+differenceofmyHeightandKoreanaverage = 1.625 - myHeightinmeters #This is the variable for the difference of my weirdly short height and the Korean average in meters.
+areaofSquare = sidelengthofSquare ** 2 #This is the variable for the area of my pretty little square described earlier.
+halfthevolumeofCubeusingmySquaredimensions = 0.5 * (sidelengthofSquare ** 3) #This is the variable for half of the volume of a cube derived from my special square.
+ninethofRectanglearea = (1.00000000000/9) * (lengthofRectangle * heightofRectangle) #This is the variable for a nineth of my previously described rectangle.
+print "I am " + str(myName) + "." " I am " + str(myAgeindecimals) + " years old" + "." " My square has the sidelength of " + str(sidelengthofSquare) + " units" + "." " I have " + str(numberofyearsIhaveleft) + " years left to live" + "." " My height (meters) corresponds to " + str(myHeightinmeters) + "." " I am a short person" + "." #Printed instructions.
+print "The area of my special square is", areaofSquare, "units", ".", "The height of my fabulous rectangle is", heightofRectangle, "units", "and the length is", lengthofRectangle, "units", ".", "A nineth of my rectangle's area is", ninethofRectanglearea, "units squared", "." #Printed instructions.
+print 10000 * ";)" #Printed instructions, like wink please.
