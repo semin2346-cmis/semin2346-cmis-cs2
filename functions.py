e2b13dc8b1e9f2906fa7d64f0127bcb2c06095fd
cmis <cmis@cmis-hs13.(none)>
2016-02-03 14:31:55 +0700

    Assignment 2.

---
+++ functions.py
@@ -0,0 +1,20 @@
+x = "2"
+y = int(x) * 45
+print y
+
+r = 6
+diameter = 2 * r
+print float(diameter) * (3 ** 2)
+print 2 * str(diameter)
+
+word = "Pneumonoultramicroscopicsilicovolcanoconiosis"
+lenword = len(word)
+print lenword
+print int(lenword) * 2
+print 2 * str(lenword)
+
+import math
+
+print math.factorial(len(word))
+print math.pi
+print len(str(math.pi)) - 2
