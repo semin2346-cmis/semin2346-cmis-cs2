aec38e7161305dbd5af6949db2f7514524cdfabc
cmis <cmis@cmis-hs13.(none)>
2016-02-05 09:55:46 +0700

    Modified.

--- functions.py
+++ functions.py
@@ -18,3 +18,18 @@ import math
 print math.factorial(len(word))
 print math.pi
 print len(str(math.pi)) - 2
+
+def need_help(name):
+	print "Please do help me %s" % name + "."
+	print "I like to write essays."
+need_help("Fred")
+
+def go_home(word,term):
+	print "I think I should go home %s" % word + "."
+	print "I want to go to sleep %s" % term + "."
+go_home("now","please")
+
+def HNNNGGGG(letra)
+	print "LIET MI GO HOM %s" % letra + "..."
+	print letra * 3
+HNNNGGGG("PLES")
