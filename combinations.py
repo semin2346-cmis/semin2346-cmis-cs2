04256fab8000ba6fdf504da99733dffa05ae8666
cmis <cmis@cmis-hs13.(none)>
2016-02-25 09:21:17 +0700

    Modified.

--- combinations.py
+++ combinations.py
@@ -1,25 +1,31 @@
 import math
 
-def factorial(x):
-	return math.factorial(x)
-
-def output(name, animal, nationality, x, y):
+def factorial(a):
+	return math.factorial(a)
+def sub(b, c):
+	return int(b) - int (c)
+def combination(d, e):
+	return factorial(d) / (factorial(e) * factorial(sub(d, e)))
+def output(name, animal, nationality, x, y, z):
 	out = """
 Greetings {}.
 I have been enlightened previously with the knowledge that a(n) {} is your favorite animal.
-You happen to be from {}, correct?
-Well, remember that the factorial of {} is {}.
+You call your home country {}, correct?
+Well, remember that the combination between {} and {} is {}.
 Farewell.
-""".format(name, animal, nationality, x, y)
+""".format(name, animal, nationality, x, y, z)
 	return out
 
 def main():
+	#Input
 	name = raw_input("What do you call yourself, stranger? \n")
 	animal = raw_input("Enlighten me, what is your favorite animal? \n")
 	nationality = raw_input("Where do you originate from? \n")
 	x = int(raw_input("Type a positive integer, please. \n"))
-	y = math.factorial(x)
-	out = output(name, animal, nationality, x, y)
+	y = int(raw_input("Type another positive integer that is of less value than the previous, please. \n"))
+	#Processing
+	z = combination(x, y)
+	#Output
+	out = output(name, animal, nationality, x, y, z)
 	print out
-
 main()
