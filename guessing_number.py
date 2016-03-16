370f8435fb62da0fdd7f6b6eb8f0eac395870c9d
cmis <cmis@cmis-hs13.(none)>
2016-03-16 11:17:14 +0700

    Finished.

--- guessing_number.py
+++ guessing_number.py
@@ -2,11 +2,11 @@ import random
 import math
 
 def sub(a, b):
-	return float(a) - float(b)
+	return int(a) - int(b)
 def add(c, d):
-	return float(c) + float(d)
+	return int(c) + int(d)
 
-def guessing(chosen_number, estimation, difference):
+def guessing_small(chosen_number, estimation, difference):
 	guess_small = """
 The target was {}.
 Your guess was {}.
@@ -14,7 +14,7 @@ That's under by {}.
 """.format(chosen_number, estimation, difference)
 	return guess_small
 
-def guessing(chosen_number, estimation, difference):
+def guessing_big(chosen_number, estimation, difference):
 	guess_big = """
 The target was {}.
 Your guess was {}.
@@ -22,7 +22,7 @@ That's over by {}.
 """.format(chosen_number, estimation, difference)
 	return guess_big
 
-def guessing(chosen_number, estimation):
+def guessing_correct(chosen_number, estimation):
 	guess_correct = """
 The target was {}.
 Your guess was {}.
@@ -32,20 +32,20 @@ That's correct.
 
 def main():
 	#Input
-	minimum = float(raw_input("What is the minimum number? "))
-	maximum = float(raw_input("What is the maximum number? "))
+	minimum = int(raw_input("What is the minimum number? "))
+	maximum = int(raw_input("What is the maximum number? "))
 	estimation = raw_input("I'm thinking of a number between " + str(minimum) + " and " + str(maximum) + ".\n" + "What do you think it is?: ")
 	#Processing
-	chosen_number = float(random.randint(minimum, maximum))
-	difference = sub(float(estimation), chosen_number)	
+	chosen_number = int(random.randint(minimum, maximum))
+	difference = sub(int(estimation), chosen_number)	
 	#Output
-	if chosen_number > str(float(estimation)):	
-		guess_small = guessing(chosen_number, estimation, difference)
+	if chosen_number > str(abs(int(estimation))):	
+		guess_small = guessing_small(chosen_number, estimation, difference)
 		print guess_small
-	elif chosen_number < str(float(estimation)):
-		guess_big = guessing(chosen_number, estimation, difference)
+	elif chosen_number < str(abs(int(estimation))):
+		guess_big = guessing_big(chosen_number, estimation, difference)
 		print guess_big
-	elif chosen_number == str(float(estimation)):
-		guess_correct = guessing(chosen_number, estimation, difference)
+	elif chosen_number == abs(int(estimation)):
+		guess_correct = guessing_correct(chosen_number, estimation)
 		print guess_correct
 main()
