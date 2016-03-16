f23ae36c8949d3425e5c8d161c97725a920af8b8
cmis <cmis@cmis-hs13.(none)>
2016-03-16 11:57:19 +0700

    Started.

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+#This game will give you 5 mental maths questions pertaining to the category you pick-addition, subtraction, multiplication, division.
+#At the end of the quiz, it will give you a score out of 5.
+
+import math
+import random
+
+def sum_questions():
+	raw_input("Please type two numbers you want the question
+
+def main()
+	print "Welcome to the conditionals mental maths game.\n Choose a category: addition, subtraction, multiplication, division."
+
