d89620bf1a93ea499ebc275a8548040a63a7c08b
cmis <cmis@cmis-hs02.(none)>
2016-03-23 11:56:20 +0700

    Modified.

--- conditionals.py
+++ conditionals.py
@@ -6,11 +6,14 @@ import random
 
 def add(a, b):
 	return int(a) + int(b)
+def mul(a, b):
+	return int(a) * int(b)
 
+#Addition quiz.
 def sum_questions():
 	#Boundaries for equation.	
-	minimum_sum = int(raw_input("Please type a minimum number for your questions. "))
-	maximum_sum = int(raw_input("Please type a maximum number for your questions. "))
+	minimum_sum = int(raw_input("Please type a minimum value for your questions. "))
+	maximum_sum = int(raw_input("Please type a maximum value for your questions. "))
 	#Variables for equation.
 	a1 = random.randint(minimum_sum, maximum_sum)
 	a2 = random.randint(minimum_sum, maximum_sum)
@@ -28,13 +31,16 @@ def sum_questions():
 	sum_answer3 = add(c1, c2)
 	sum_answer4 = add(d1, d2)
 	sum_answer5 = add(e1, e2)
+	#Variable for grading.
+	numbercorrect = 0
 	print "None of the numbers used in the questions will exceed the set extremes."
 	#Question 1.
 	sum1 = str(a1) + "+" + str(a2)
 	print sum1	
 	estimation_sum1 = raw_input("Answer to question 1: ")	
 	if int(estimation_sum1) == int(sum_answer1):
-		print "Correct."
+		numbercorrect = numbercorrect + 1		
+		print "Correct."	
 	else:
 		print "Incorrect."
 	#Question 2.
@@ -42,6 +48,7 @@ def sum_questions():
 	print sum2
 	estimation_sum2 = raw_input("Answer to question 2: ")	
 	if int(estimation_sum2) == int(sum_answer2):
+		numbercorrect = numbercorrect + 1
 		print "Correct."
 	else:
 		print "Incorrect."
@@ -50,6 +57,7 @@ def sum_questions():
 	print sum3
 	estimation_sum3 = raw_input("Answer to question 3: ")	
 	if int(estimation_sum3) == int(sum_answer3):
+		numbercorrect = numbercorrect + 1
 		print "Correct."
 	else:
 		print "Incorrect."
@@ -58,34 +66,110 @@ def sum_questions():
 	print sum4	
 	estimation_sum4 = raw_input("Answer to question 4: ")
 	if int(estimation_sum4) == int(sum_answer4):
+		numbercorrect = numbercorrect + 1
 		print "Correct."
 	else:
 		print "Incorrect."	
 	#Question 5.
-	sum5 = str(e1) + "+" + str(e1)
+	sum5 = str(e1) + "+" + str(e2)
 	print sum5	
 	estimation_sum5 = raw_input("Answer to question 5: ")
 	if int(estimation_sum5) == int(sum_answer5):
+		numbercorrect = numbercorrect + 1
 		print "Correct."
 	else:
 		print "Incorrect."
 	#Scoring.
-	numbercorrect = 0
-	if int(estimation_sum1) == int(sum_answer1):
-		numbercorrect = numbercorrect + 1
-	elif int(estimation_sum2) == int(sum_answer2):
-		numbercorrect = numbercorrect + 1
-	elif int(estimation_sum3) == int(sum_answer3):
-		numbercorrect = numbercorrect + 1
-	elif int(estimation_sum4) == int(sum_answer4):
-		numbercorrect = numbercorrect + 1
-	elif int(estimation_sum5) == int(sum_answer5):
-		numbercorrect = numbercorrect + 1
-	elif numbercorrect >= 3:
-		print "You passed."
+	if numbercorrect >= 3:
+		print "You passed. Your score is " + str(numbercorrect) + " out of 5."
 	else:
-		print "You failed."
-sum_questions()
+		print "You failed. Your score is " + str(numbercorrect) + " out of 5."
+
+#Multiplication quiz.
+def product_questions():
+	#Boundaries for equation.	
+	minimum_product = int(raw_input("Please type a minimum value for your questions. "))
+	maximum_product = int(raw_input("Please type a maximum value for your questions. "))
+	#Variables for equation.
+	f1 = random.randint(minimum_product, maximum_product)
+	f2 = random.randint(minimum_product, maximum_product)
+	g1 = random.randint(minimum_product, maximum_product)
+	g2 = random.randint(minimum_product, maximum_product)
+	h1 = random.randint(minimum_product, maximum_product)
+	h2 = random.randint(minimum_product, maximum_product)
+	i1 = random.randint(minimum_product, maximum_product)
+	i2 = random.randint(minimum_product, maximum_product)
+	j1 = random.randint(minimum_product, maximum_product)
+	j2 = random.randint(minimum_product, maximum_product)
+	#Answers for equation.
+	product_answer1 = mul(f1, f2)
+	product_answer2 = mul(g1, g2)
+	product_answer3 = mul(h1, h2)
+	product_answer4 = mul(i1, i2)
+	product_answer5 = mul(j1, j2)
+	#Variable for grading.
+	numbercorrect = 0
+	print "None of the numbers used in the questions will exceed the set extremes."
+	#Question 1.
+	product1 = str(f1) + "*" + str(f2)
+	print product1	
+	estimation_product1 = raw_input("Answer to question 1: ")	
+	if int(estimation_product1) == int(product_answer1):
+		numbercorrect = numbercorrect + 1		
+		print "Correct."	
+	else:
+		print "Incorrect."
+	#Question 2.
+	product2 = str(g1) + "*" + str(g2)
+	print product2
+	estimation_product2 = raw_input("Answer to question 2: ")	
+	if int(estimation_product2) == int(product_answer2):
+		numbercorrect = numbercorrect + 1
+		print "Correct."
+	else:
+		print "Incorrect."
+	#Question 3.
+	product3 = str(h1) + "*" + str(h2)
+	print product3
+	estimation_product3 = raw_input("Answer to question 3: ")	
+	if int(estimation_product3) == int(product_answer3):
+		numbercorrect = numbercorrect + 1
+		print "Correct."
+	else:
+		print "Incorrect."
+	#Question 4.	
+	product4 = str(i1) + "*" + str(i2)
+	print product4	
+	estimation_product4 = raw_input("Answer to question 4: ")
+	if int(estimation_product4) == int(product_answer4):
+		numbercorrect = numbercorrect + 1
+		print "Correct."
+	else:
+		print "Incorrect."	
+	#Question 5.
+	product5 = str(j1) + "*" + str(j2)
+	print product5	
+	estimation_product5 = raw_input("Answer to question 5: ")
+	if int(estimation_product5) == int(product_answer5):
+		numbercorrect = numbercorrect + 1
+		print "Correct."
+	else:
+		print "Incorrect."
+	#Scoring.
+	if numbercorrect >= 3:
+		print "You passed. Your score is " + str(numbercorrect) + " out of 5."
+	else:
+		print "You failed. Your score is " + str(numbercorrect) + " out of 5."
+
+#Main function.
 def main():
 	print "Welcome to the conditionals mental maths game.\n"
-	print raw_input("Choose a category: addition, subtraction, multiplication, division.")
+	z = raw_input("Choose a category: addition, subtraction, multiplication, division. ")
+	print z
+	if z == "addition":
+		sum_questions()
+	elif z == "multiplication":
+		product_questions()
+	else:
+		print "Dude, you had one job. How can you already fail?"
+main()
