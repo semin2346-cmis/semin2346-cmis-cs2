0f4ec9a66327d9b7a142ce3956ca07296fee7b2c
cmis <cmis@cmis-hs02.(none)>
2016-04-22 09:59:08 +0700

    Modified.

--- recursion.py
+++ recursion.py
@@ -1,18 +1,32 @@
-n = int(raw_input("Type number. "))
-o = int(raw_input("Type bigger number. "))
-
+#n = int(raw_input("Type number: "))
 #def countup(n):
-#	if n <= 10:
+#	if n > 10:
 #		print "Ye."
 #	else:
 #		print n
 #		countup(n + 1)
 #countup(n)
 
-def countup(start, stop):
-	if start == stop:
-		print "Ye."
+#def countup(start, stop):
+#	if start > stop:
+#		print "Ye.\n"
+#	else:
+#		print start
+#		countup(start + 1, stop)
+#countup(4, 10)
+
+#def countdown(start, stop):
+#	if start < stop:
+#		print "Ye."
+#	else:
+#		print start
+#		countdown(start - 1, stop)
+#countdown(10, 4)
+
+def adder(s): 
+	s = raw_input("Next number: ")
+	if s == '':
+		print "No."
 	else:
-		print start
-		countup(start + 1, stop)
-countup(1, 5)
+		adder(s)
+adder("s")
