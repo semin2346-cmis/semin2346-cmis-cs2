f31257c28c10e52766b2f68a73a551384387bc4a
cmis <cmis@cmis-hs02.(none)>
2016-05-17 09:55:29 +0700

    Modified.

--- guessing_number_with_recursion.py
+++ guessing_number_with_recursion.py
@@ -3,12 +3,19 @@ import math
 
 def estimation(a, correct):
 	guess = raw_input("I'm thinking of a number between " + str(0) + " and " + str(100) + ".\n" + "What do you think it is?: ") 
-	if guess == correct:
-		return "Good guess."
+	if int(guess) == int(correct):
+		print "Good guess."
 	else:
-		print "Try again."
-		print correct
-		return estimation(0, correct) 
+		if int(guess) < int(correct):
+			print "Try again. Your guess is too small."
+			print correct
+			return estimation(0, correct) 
+		elif int(guess) > int(correct):
+			print "Try again. Your guess is too large."
+			print correct
+			return estimation(0, correct) 
+		else:
+			print "How. HOW could you get that wrong?"
 estimation(0, random.randint(0, 100))
 
 #def sub(a, b):
@@ -59,3 +66,70 @@ estimation(0, random.randint(0, 100))
 #		guess_correct = guessing_correct(chosen_number, estimation)
 #		print guess_correct
 # main()
+
+import random
+import math
+
+def stage(correct, times_recursed, response):
+	guess = raw_input("I'm thinking of a number between " + str(0) + " and " + str(100) + ".\n" + "What do you think it is?: ")
+	if correct == int(guess):
+		print "Good guess!"
+		return response + 1
+	elif times_recursed == 0:
+		return response
+	elif correct < int(guess):
+		print "Try again. Your guess is too small."
+		return stage(correct, times_recursed - 1, response)
+	elif correct > int(guess):
+		print "Try again. Your guess is too small."
+		return stage(correct, times_recursed - 1, response)
+	else:
+		print "How. HOW could you get that wrong?"
+
+def round_number(n, response, computers_correct):	
+	if n == 0:
+		print """
+{} {}
+""".format(str(response), str(computers_correct))
+	else:
+		correct = random.randint(1, 100)
+		print "\nYou have {} rounds left".format(n)
+		response = stage(correct, 4, response)
+		highest_number = 100
+		lowest_number = 0
+		number = 50
+		round_number(n - 1, uc, computers_correct)
+
+def main():
+	round_number(3, 0, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bonus(highest_number, lowest_number, number, computers_correct, number_recursed):
+	print "\nI guess {}".format(number)
+	your_number = raw_input("too (h)igh, too (l)ow, (c)orrect: ")
+	if your_number == "c":
+		return computers_correct + 1
+	elif number_recursed == 0:
+		return computers_correct
+	elif your_number == "h":
+		bonus(number, lowest_number, ((number - highest_number) / 2) + number, computers_correct, number_recursed - 1)
+	else:
+		bonus(highest_number, number, ((highest_number - number) / 2) + number, computers_correct, number_recursed - 1)
+
+
