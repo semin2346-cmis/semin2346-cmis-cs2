5b816f1d92e381e5d2c03d6f3fe2724c2005b925
cmis <cmis@cmis-hs02.(none)>
2016-05-26 14:35:58 +0700

    Modified.

--- whileloops.py
+++ whileloops.py
@@ -4,11 +4,76 @@
 #		x -= 1
 #countdown(10)
 
-def counter(x):
-	while x <= 0:
+#def counter(x):
+#	while x <= 0:
+#		print x
+#		x += 1
+#		if x == y:
+#			print y
+#	while x >= 0:
+#		print x
+#		x -= 1
+#		if x == y:
+#			print y
+#counter(-6)
+
+#def counting(x, y):
+#	while x < y:
+#		print x
+#		x += 1
+#		if x == y:
+#			print y
+#	while x > y:
+#		print x
+#		x -= 1
+#		if x == y:
+#			print y
+#counting(9, 2)
+#counting(2, 9)
+
+def sum_of_odds(x, y = 0):
+	while x < 0:
 		print x
 		x += 1
-	while x >= 0:
+		while x % 2 == 1:
+			print sum_of_odds(x + x, y + 1)
+	while x > 0:
 		print x
 		x -= 1
-counter(-6)
+		while x % 2 == 1:
+			print sum_of_odds(x + x, y + 1)
+sum_of_odds(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
